31de3b95661d7976cc0013174026c37aece47400
Ivan Semikin <i.semikin.94@icloud.com>
2024-07-22 22:20:16 +0200

    Initial Commit

---
+++ IAmRich/IAmRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichApp.swift
+//  IAmRich
+//
+//  Created by Иван Семикин on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
